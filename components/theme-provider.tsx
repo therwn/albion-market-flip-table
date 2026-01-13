7419a8d3438929055ceccc55edbe53521ba7b82d
therwn <therwn0@gmail.com>
2026-01-13 14:34:46 +0300

    Fix: Use React.ComponentProps for ThemeProvider type inference

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,13 +3,7 @@
 import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
-interface ThemeProviderProps {
-  children: React.ReactNode;
-  attribute?: string;
-  defaultTheme?: string;
-  enableSystem?: boolean;
-  disableTransitionOnChange?: boolean;
-}
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
